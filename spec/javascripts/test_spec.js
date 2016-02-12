7643b3fc5ff87f215243973a40157b72733740ec
LorenzoBaracchi <baracchi.lorenzo@gmail.com>
2016-02-12 14:57:19 +0100

    minor

--- spec/javascripts/test_spec.js
+++ spec/javascripts/test_spec.js
@@ -2,6 +2,9 @@ describe("My great feature", function() {
 
   it("will change the world", function() {
     expect(true).to.equal(true);
+  });
+
+  it("should have jquery", function() {
     expect($).to.not.be.undefined;
   });
 
