761dcc024fd8c114cdbdb1299a853789e3876345
LorenzoBaracchi <baracchi.lorenzo@gmail.com>
2016-02-17 14:17:16 +0100

    tests

--- spec/javascripts/people_spec.js
+++ spec/javascripts/people_spec.js
@@ -4,8 +4,28 @@ describe('People', function() {
   });
 
   it('should have a div with pippo', function() {
-    console.log($('#1'));
     expect($('#1').text()).to.have.string('pippo');
     expect($('#1').text()).to.have.string('Age:42');
   });
+
+
+  it('should toggle bio visibility', function() {
+    expect(isToShow(1)).to.equal(true);
+
+    showBio(1);
+
+    expect(isToShow(1)).to.equal(false);
+
+    hideBio(1);
+
+    expect(isToShow(1)).to.equal(true);
+  });
+
+  it('should set bio description', function() {
+    expect($('#1 .bio .bio_content').text()).to.equal('');
+
+    setBio(1, 'a beatiful description');
+    expect($('#1 .bio .bio_content').text()).to.equal('a beatiful description');
+  });
+
 });
